0f1d5f14d91335f355f16f9576438d05a01b8de6
freecat <kenkencancan2002@gmail.com>
2019-07-23 01:25:15 +0900

    add probackward

--- ex1.cpp
+++ ex1.cpp
@@ -6,6 +6,7 @@ struct ex1
     /* data */
     double w_;
     double b_;
+    double input_, output_;
 };
       
 
@@ -15,16 +16,47 @@ double getAct(const double& x)
     //return MAX2()
 }
 
-double feedForward(const double& input, ex1 ex1)
+double getActivation(const double& x)
+{
+    return x;
+    //return MAX2()
+}
+
+double getActGrad(const double& x)
+{
+    return 1.0;
+}
+
+ex1 propBackward(const double& target,ex1 ex1)
+{
+    const double alpha = 0.1;
+    
+    const double grad = (ex1.output_ - target) * getActGrad(ex1.output_);
+
+    ex1.w_ -= alpha * grad * ex1.input_;
+    ex1.b_ -= alpha * grad * 1.0;
+    return ex1;
+}
+
+
+// void feedForwardandPrint(const double& input, ex1 ex1)
+// {
+//  printf("%f %f \n",input, feedForward(input, ex1));
+// }
+
+ex1 feedForward(const double& input, ex1 ex1)
 {
     // output y = f(\sigma)
     //\sigma = w?* input x + b
     // for mulitiple  input
     // \sigma w0_ * x0_ + w1_ * x1 + ... + b
+    ex1.input_ = input;
+    
+    const double sigma = ex1.w_ *  ex1.input_+ ex1.b_;
 
-    const double sigma = ex1.w_ * input + ex1.b_;
+    ex1.output_ = getAct(sigma);
 
-    return getAct(sigma);
+    return ex1;
 }
 
 int main()
@@ -43,12 +75,14 @@ int main()
     // output is outcome or input of other neuron
 
     //double input;
-    double output;
+    //double output;
+for(int r=0; r < 100; r++){
+    myneuron = feedForward(1.0, myneuron);
+    printf("%f %f \n", myneuron.input_,myneuron.output_);
 
-    printf("%f %f \n",0.0, feedForward(0.0, myneuron));
-    printf("%f %f \n",1.0, feedForward(1.0, myneuron));
-    printf("%f %f \n",2.0, feedForward(2.0, myneuron));
-
+    myneuron =  propBackward(4.0, myneuron);
+    printf("%f %f \n",myneuron.w_,myneuron.b_);
+}
     // process in neuron sigma(sum) activation
     // sum = w * x+ b
     // ex, x= 0.5, w =2 , b= 1 sum = w * x + b -> sum = 2
